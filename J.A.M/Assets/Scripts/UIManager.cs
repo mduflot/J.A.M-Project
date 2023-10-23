04be37cdb85ab381cc03f8e3f6f7730069b631dd
ElSachs <75885665+ElSachs@users.noreply.github.com>
2023-10-23 20:47:04 +0200

    small fixes

--- J.A.M/Assets/Scripts/UIManager.cs
+++ J.A.M/Assets/Scripts/UIManager.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public Image[] gauges;
+    public Gauges[] gauges;
     private Dictionary<SpaceshipManager.System, Image> gaugeReferences = new Dictionary<SpaceshipManager.System, Image>();
+    
+    [Serializable] 
+    public struct Gauges
+    {
+        public SpaceshipManager.System system;
+        public Image gauge;
+    }
+
+    private void Start()
+    {
+        InitializeGauges();
+    }
 
     private void InitializeGauges()
     {
-        /*for (int i = 0; i < gauges.Length; i++)
+        for (int i = 0; i < gauges.Length; i++)
         {
-            gaugeReferences.Add(, gauges[i]);
-        }*/
+            gaugeReferences.Add(gauges[i].system, gauges[i].gauge);
+        }
     }
     public void UpdateGauges(SpaceshipManager.System system, float value)
     {
