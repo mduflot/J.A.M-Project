28791070ec4d162100b40c847da253538f8e8c1f
Alexandre Mocoeur <alexandremocoeur@gmail.com>
2023-11-04 18:29:03 +0100

    feat : finished Checker protoCode

--- J.A.M/Assets/Scripts/Checker/Checker.cs
+++ J.A.M/Assets/Scripts/Checker/Checker.cs
@@ -2,122 +2,171 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checker
-{ 
-    public static void GenerateRandomEvent()
-    {
-        /*
-         * var :
-         */
-        //activeStorylines : List<StoryLines>, list of currently active Storylines (max 3)
-        //inactiveStoryLines : List<StoryLines>, list of currently inactive Storylines
-        //priorityFactor : float, factor for active StoryLine selection
-        //pickPercent : float, % chance for an outcome
-        //weighedActivePercent : float, probability for an active timeline to be selected
-        //weighedInactivePercent : float, probability for an inactive timeline to be selected
-        //randPicker : float, random value used to chose among available or unavailable storylines
-        
-        /*
-         * Algorithm
-         */
-        
-        //if activeStorylines.length < 1 then
-        //   ChoseNewStoryline()
-        //else
-        //   pickPercent = 100.0/3.0;
-        //   weighedInactivePercent = pickPercent * ( 1 - 1.0/priorityFactor)
-        //   weighedActivePercent = 1 - weighedInactivePercent 
-        //   randPicker = random(0,100)
-        //   if randPicker <= weighedInactivePercent then :
-        //      ChoseNewStoryLine()
-        //   else
-        //      if randPicker <= (weighedActivePercent / 2.0) + weighedInactivePercent then : 
-        //         PickEventFromActiveStoryline(activeStoryline[0])
-        //      else
-        //         PickEventFromActiveStoryline(activeStoryline[1])
-        
-    }
- 
-    public static void ChoseNewStoryline()
-    {
-        /*
-         * var : 
-         */
-        //choseTimer : float, time elapsed since beginning | last storyline check
-        //choseThreshold : float, time that has to elapse between two SL checks
-        //StoryLines : List<StoryLines>, list of all storylines
-        //availableStorylines : List<Storylines>, stores available storylines
-        //numberOfASL : int, number of available storylines
-        //pickPercent : float, % chance for a single Storyline to be chosen (supposing equiprobability)
-        //randPicker : float, random value used to chose among available storylines
-        //i : int, value used in for loop
-        
-        /*
-         * Algorithm
-         */
-        
-        //if choseTimer > choseThreshold then :
-        //  foreach(var sl in Storylines) do :
-        //      if(sl is available) then :
-        //          availableStoryLines.add(sl)
-        
-        //numberOfASL = availableStoryLines.length
-        //pickPercent = 100.0/numberOfASL
-        //randPicker = random(0.0,100.0)
-        
-        //for i = 1, i <= numberOfASL, i++
-        //  if randPicker <= pickPercent * i
-        //      StartStoryLine(availableStoryLines[i-1]);
-    }
+/*
+ * NEEDED INFORMATION FOR CHECKER :
+ *
+ * STORYLINES :
+ *    - What is the maximum number of concurrent storylines ? (assumed to be 3 for now)
+ *    - Where are they stored ? 
+ *    - How do you check if they're active or not ?
+ *    - How do you store their conditions ?
+ *    - How do we calculate probability for a Storyline to be picked ? (Currently uses a priority factor shared by all active Storylines)
+ *      FOR NOW : PROB FACTOR
+ *
+ * TIMELINES :
+ *    - How are they stored ?
+ *    - How do you store their conditions ?
+ *      SAME AS STORYLINES
+ *    - How are storylines conditions affected by timelines stored and affected ? (see Miro graph Storyline exemple -> "Affect conditions")
+ *      SOME TIMELINES ARE LOCKED UNTIL OTHERS HAVE PLAYED (bool hasPlayed on some timelines)
+ *    - How is the first task of a timeline stored ?
+ * 
+ * CONDITIONS :
+ *    - What are Storylines conditions ? -> Add "Traits" as "StorylinesConditions" ? Use preexisting traits ?
+ *      GAUGE CONDITIONS / BOOLEANS ON SHIP TRAITS OR TRAITS (SHIPTRAITS NOT IMPLEMENTED FOR NOW -> TRUE)
+ *    - Are the timelines conditions picked from the same set ?
+ *    - Should they compare to var data from characters (mood, stress) ? -> Add TraitSystem function to generate flags based on charVarData
+ *
+ */
 
-    public static void PickEventFromActiveStoryline()
-    {
-        /*
-         * var :
-         */
-        
-        /*
-         * Algorithm
-         */
-    }
-    
-    public static void CheckOutcome()
-    {
-        
-        /*
-         * var :
-         */
-        /*FLAGS*/
-        //outJF : Job, Job flags that affect outcome
-        //outPF : PositiveTraits, Positive Trait flags that affect outcome
-        //outNF : NegativeTraits, Negative Trait flags that affect outcome
-        //CJF : Job, character job flag(s)
-        //CPF : PositiveTraits, character Positive trait flag(s)
-        //CNF : NegativeTraits, character Negative trait flag(s)
-        //matchedJF : Job, result of TraitSystem.MatchJobFlags(CJF, outJF)
-        //matchedPF : PositiveTraits, result of TraitSystem.MatchPositiveFlags(CPF, outPF)
-        //matchedNF : NegativeTraits, result of TraitSystem.MatchNegativeFlags(CNF, outNF)
-        
-        //isOutcomePositive : bool, store value for "end condition matched" check
-        //finalOutcome : TaskOutcome, final outcome chosen based on parameters & conditions
-        //outComeMap : dictionary<string, 
-        
-       /*
-        * Algorithm
-        */
-        
-        //isOutcomePositive = some result based on state of task
-        
-        //matchedJF = TraitSystem.MatchJobFlags(CJF, outJF)
-        //matchedPF = TraitSystem.MatchPositiveFlags(CPF, outPF) 
-        //matchedNF = TraitSystem.MatchNegativeFlags(CNF, outNF)
-        
- 
-        
-        
-        /*
-         * Operation on level outcome
-         */
-        
-    }
+public class Checker
+{
+ //currently assumes that the max number of storylines is 3
+ public static void GenerateRandomEvent()
+ {
+  /*
+   * parameters : 
+   */
+  //activeStorylines : List<StoryLines>, list of currently active Storylines (max 3)
+  //inactiveStoryLines : List<StoryLines>, list of currently inactive Storylines
+  //priorityFactor : float, factor for active StoryLine selection
+  
+  /*
+   * var :
+   */
+  //pickPercent : float, % chance for an outcome
+  //weighedActivePercent : float, probability for an active timeline to be selected
+  //weighedInactivePercent : float, probability for an inactive timeline to be selected
+  //randPicker : float, random value in [0,100] used to chose among available or unavailable storylines
+
+  /*
+   * Algorithm : 
+   */
+
+  //if activeStorylines.length < 1 then
+  //   ChoseNewStoryline()
+  //else
+  //   pickPercent = 1.0/3.0;
+  //   weighedInactivePercent = pickPercent * ( 1 - 1.0/priorityFactor)
+  //   weighedActivePercent = 1 - weighedInactivePercent 
+  //   randPicker = random(0,1)
+  //   if randPicker <= weighedInactivePercent then :
+  //      ChoseNewStoryLine(inactiveStorylines)
+  //   else
+  //      if randPicker <= (weighedActivePercent / 2.0) + weighedInactivePercent then : 
+  //         PickEventFromActiveStoryline(activeStoryline[0])
+  //      else
+  //         PickEventFromActiveStoryline(activeStoryline[1])
+
+ }
+
+ private static void ChoseNewStoryline()
+ {
+  /*
+   * parameters :
+   */
+  //inactiveStorylines : List<StoryLines>, list of all inactive storylines
+  //conditions : Traits, current set of conditions to match to Storylines conditions
+  
+  /*
+   * var :
+   */
+  //availableStorylines : List<Storylines>, stores available storylines based on their conditions
+  //numberOfASL : int, number of available storylines
+  //pickPercent : float, % chance for a single Storyline to be chosen (supposing equiprobability)
+  //randPicker : float, random value in [0,100] used to chose among available storylines
+  //i : int, index used in for loop
+
+  /*
+   * Algorithm :
+   */
+
+  //foreach(var sl in Storylines) do :
+  //   if conditions match sl then :
+  //       availableStoryLines.add(sl)
+
+  //numberOfASL = availableStoryLines.length
+  //pickPercent = 100.0/numberOfASL
+  //randPicker = random(0.0,100.0)
+
+  //for i = 1, i <= numberOfASL, i++
+  //  if randPicker <= pickPercent * i
+  //      StartStoryLine(availableStoryLines[i-1]);
+  //      break
+ }
+
+ private static void PickTimelineFromActiveStoryline()
+ {
+  /*
+   * parameters :
+   */
+  //chosenStoryline : Storyline, Storyline to pick Timelines from
+  //conditions : Traits, Traits Tuple giving current state of ship / available characters / etc..
+  
+  /*
+   * var :
+   */
+  //availableTimelines : List<Timeline>, list of all available timelines in the current Storyline based on conditions
+  //numberOfTL : int, number of available Timelines
+  //pickPercent : float, % chance of a single timeline being chosen (based on equiprobability)
+  //randPicker : float, random value in [0,100] used to chose among available Timelines 
+  //i : int, index used in for loop
+  
+  
+  /*
+   * Algorithm :
+   */
+  
+  //for each Timeline in chosenStoryline do :
+  //   if conditions match Timeline then :
+  //      availableTimelines.add(Timeline)
+  //
+  //numberOfASL = availableTimelines.length
+  //pickPercent = 100.0/numberOfAsl
+  //randPicker = random(0,100)
+  //
+  //for i = 1, i <= numberOfASL, i++ do :
+  //   if randPicker <= pickPercent * i then :
+  //      PlayTimeline(availableTimelines[i-1])
+  //      break;
+ }
+
+ private static void StartStoryline()
+ {
+  /*
+   * parameters : 
+   */
+  //chosenStoryline : StoryLine, Storyline to start
+  
+  /*
+   * Algorithm :
+   */
+  //choseStoryline.setActive(true)
+  //PickTimelineFromActiveStoryline(chosenStoryline)
+  
+ }
+
+ private static void PlayTimeline()
+ {
+  /*
+   * parameters :
+   */
+  //chosenTimeline : TimeLine, Timeline to play
+  
+  /*
+   * Algorithm :
+   */
+  //chosenTimeline.setActive(true)
+  //choseTimeline.StartFirstTask() // Supposes that task start is handled outside of checker script 
+ }
 }
