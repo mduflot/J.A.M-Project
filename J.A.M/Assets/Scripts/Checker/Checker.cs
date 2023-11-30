200a60df4cc42aa4660982a7901105318fe77976
Maxime <32262840+duflosky@users.noreply.github.com>
2023-11-30 09:52:20 +0100

    feat: add logic in checker;

--- J.A.M/Assets/Scripts/Checker/Checker.cs
+++ J.A.M/Assets/Scripts/Checker/Checker.cs
@@ -1,163 +1,144 @@
 ﻿using System.Collections.Generic;
 using SS;
+using SS.Enumerations;
 using SS.ScriptableObjects;
 using UnityEngine;
 
 public class Checker : MonoBehaviour
 {
-    //activeStorylines : List<StoryLines>, list of currently active Storylines (max 3)
-    //inactiveStoryLines : List<StoryLines>, list of currently inactive Storylines
-    //priorityFactor : float, factor for active StoryLine selection
-    private List<Storyline> allStorylines;
+    private SSCampaignSO campaign;
     private List<Storyline> activeStorylines;
-    private List<Storyline> inactiveStorylines;
     private float priorityFactor;
-    
-    private Storyline chosenStoryline;
-    
-    //pickPercent : float, % chance for an outcome
-    //weighedActivePercent : float, probability for an active timeline to be selected
-    //weighedInactivePercent : float, probability for an inactive timeline to be selected
-    //randPicker : float, random value in [0,100] used to chose among available or unavailable storylines
-    float pickPercent, weighedActivePercent, weighedInactivePercent, randPicker;
 
-    private void Initialize(SSCampaignSO campaign)
+    private Storyline chosenStoryline;
+    private SSNodeGroupSO chosenTimeline;
+
+    // TODO : Revoir les probabilités de l'ensemble du script
+    // TODO : Revoir la gestion du choix de la storyline
+    
+    public void Initialize(SSCampaignSO ssCampaign)
     {
-        allStorylines = new List<Storyline>();
-        foreach (var storyline in campaign.Storylines)
-        {
-            
-        }
+        campaign = ssCampaign;
     }
 
     public void GenerateRandomEvent()
     {
         if (activeStorylines.Count < 3)
         {
-            pickPercent = 1.0f/3.0f;
-            weighedInactivePercent = pickPercent * (1 - 1.0f / priorityFactor);
-            weighedActivePercent = 1 - weighedInactivePercent;
-            randPicker = Random.Range(0, 1);
+            //pickPercent : float, % chance for an outcome
+            //weighedActivePercent : float, probability for an active timeline to be selected
+            //weighedInactivePercent : float, probability for an inactive timeline to be selected
+            //randPicker : float, random value in [0,100] used to chose among available or unavailable storylines
+            var pickPercent = 1.0f / 3.0f;
+            var weighedInactivePercent = pickPercent * (1 - 1.0f / priorityFactor);
+            var weighedActivePercent = 1 - weighedInactivePercent;
+            var randPicker = Random.Range(0, 1);
+            if (activeStorylines.Count == 0)
+            {
+                ChooseNewStoryline(campaign.ActivatableStorylines);
+                PickTimelineFromStoryline();
+                return;
+            }
+
             if (randPicker <= weighedInactivePercent)
             {
-                ChooseNewStoryline(inactiveStorylines);
+                ChooseNewStoryline(campaign.ActivatableStorylines);
+                PickTimelineFromStoryline();
             }
             else
             {
+                // TODO : ???
                 if (randPicker <= (weighedActivePercent / 2.0) + weighedInactivePercent)
                 {
-                    PickTimelineFromStoryline();
+                    PickTimelineFromStoryline(activeStorylines);
                 }
                 else
                 {
-                    PickTimelineFromStoryline();
+                    PickTimelineFromStoryline(activeStorylines);
                 }
             }
         }
         else
         {
-            PickTimelineFromStoryline();
+            PickTimelineFromStoryline(activeStorylines);
         }
     }
 
-    public void PickTimelineFromStoryline()
+    private void ChooseNewStoryline(List<Storyline> activatableStorylines = null)
+    {
+        /*
+         * parameters :
+         */
+        //conditions : Traits, current set of conditions to match to Storylines conditions
+        
+        List<Storyline> availableStorylines = new List<Storyline>();
+
+        //numberOfASL = availableStoryLines.length
+        //pickPercent = 100.0/numberOfASL
+        //randPicker = random(0.0,100.0)
+        var numberOfASL = availableStorylines.Count;
+        var pickPercent = 100.0f / numberOfASL;
+        var randPicker = Random.Range(0, 100);
+
+        foreach (var availableStoryline in activatableStorylines)
+        {
+            // TODO : Add conditions check
+        }
+
+        for (int i = 0; i < numberOfASL; i++)
+        {
+            if (randPicker <= pickPercent * i)
+            {
+                chosenStoryline = availableStorylines[i];
+                chosenStoryline.StorylineContainer.StoryStatus = SSStoryStatus.Activated;
+                activeStorylines.Add(chosenStoryline);
+                StartStoryline();
+                break;
+            }
+        }
+    }
+
+    private void PickTimelineFromStoryline(List<Storyline> activeStoryline = null)
     {
         /*
          * parameters :
          */
         //conditions : Traits, Traits Tuple giving current state of ship / available characters / etc..
 
-        /*
-         * variables :
-         */
-        //availableTimelines : List<Timeline>, list of all available timelines in the current Storyline based on conditions
-        //numberOfTL : int, number of available Timelines
-        //pickPercent : float, % chance of a single timeline being chosen (based on equiprobability)
-        //randPicker : float, random value in [0,100] used to chose among available Timelines 
-        //i : int, index used in for loop
+        if (activeStorylines != null)
+        {
+            chosenStoryline = activeStorylines[Random.Range(0, activeStorylines.Count)];
+        }
 
+        var availableTimelines = new List<SSNodeGroupSO>();
 
-        /*
-         * Algorithm :
-         */
+        foreach (var nodeGroup in chosenStoryline.ActivatableTimelines)
+        {
+            // TODO : Add conditions check
+        }
 
-        //for each Timeline in chosenStoryline do :
-        //   if conditions match Timeline then :
-        //      availableTimelines.add(Timeline)
-        //
-        //numberOfASL = availableTimelines.length
-        //pickPercent = 100.0/numberOfAsl
-        //randPicker = random(0,100)
-        //
-        //for i = 1, i <= numberOfASL, i++ do :
-        //   if randPicker <= pickPercent * i then :
-        //      PlayTimeline(availableTimelines[i-1])
-        //      break;
+        var numberOfASL = availableTimelines.Count;
+        var pickPercent = 100.0f / numberOfASL;
+        var randPicker = Random.Range(0, 100);
+
+        for (int i = 0; i < numberOfASL; i++)
+        {
+            if (randPicker <= pickPercent * i)
+            {
+                chosenTimeline = availableTimelines[i];
+                PlayTimeline();
+                break;
+            }
+        }
     }
 
-    private void ChooseNewStoryline(List<Storyline> inactiveStorylines = null)
+    private void StartStoryline()
     {
-        /*
-         * parameters :
-         */
-        //conditions : Traits, current set of conditions to match to Storylines conditions
-
-        /*
-         * variables :
-         */
-        //availableStorylines : List<Storylines>, stores available storylines based on their conditions
-        //numberOfASL : int, number of available storylines
-        //pickPercent : float, % chance for a single Storyline to be chosen (supposing equiprobability)
-        //randPicker : float, random value in [0,100] used to chose among available storylines
-        //i : int, index used in for loop
-
-        /*
-         * Algorithm :
-         */
-
-        //foreach(var sl in Storylines) do :
-        //   if conditions match sl then :
-        //       availableStoryLines.add(sl)
-
-        //numberOfASL = availableStoryLines.length
-        //pickPercent = 100.0/numberOfASL
-        //randPicker = random(0.0,100.0)
-
-        //for i = 1, i <= numberOfASL, i++
-        //  if randPicker <= pickPercent * i
-        //      StartStoryLine(availableStoryLines[i-1]);
-        //      break
+        PickTimelineFromStoryline();
     }
 
-    public void StartStoryline()
+    private void PlayTimeline()
     {
-        /*
-         * parameters :
-         */
-
-        //chosenStoryline : StoryLine, Storyline to start
-
-        /*
-         * Algorithm :
-         */
-
-        //choseStoryline.setActive(true)
-        //PickTimelineFromActiveStoryline(chosenStoryline)
-    }
-
-    public void PlayTimeline()
-    {
-        /*
-         * parameters :
-         */
-
-        //chosenTimeline : TimeLine, Timeline to play
-
-        /*
-         * Algorithm :
-         */
-
-        //chosenTimeline.setActive(true)
-        //choseTimeline.StartFirstTask() // Supposes that task start is handled outside of checker script 
+        // chosenTimeline // Supposes that task start is handled outside of checker script 
     }
 }
