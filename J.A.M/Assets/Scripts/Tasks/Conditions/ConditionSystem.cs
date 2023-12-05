a70e8c69c1a1e70d37d5c3ed59f1a1e466b436d5
Alexandre Mocoeur <alexandremocoeur@gmail.com>
2023-12-05 11:28:45 +0100

    update: added Condition Environment Check
    Checks Crew Traits & Spaceship flags

--- J.A.M/Assets/Scripts/Tasks/Conditions/ConditionSystem.cs
+++ J.A.M/Assets/Scripts/Tasks/Conditions/ConditionSystem.cs
@@ -30,22 +30,23 @@ public class ConditionSystem
         /*Additionnal Condition Check*/
 
         bool supplementaryCondition = true;
+        //FIX : turn into for loop
         foreach (var cond in taskCondition.supplementaryConditions)
         {
-            if (!CheckJob(characterTraits.GetJob(), taskCondition.BaseCondition.Traits.GetJob()))
+            if (!CheckJob(characterTraits.GetJob(), cond.Value.Traits.GetJob()))
                 supplementaryCondition = false;
 
             if (!CheckPositiveTraits(characterTraits.GetPositiveTraits(),
-                    taskCondition.BaseCondition.Traits.GetPositiveTraits()))
+                    cond.Value.Traits.GetPositiveTraits()))
                 supplementaryCondition = false;
 
             if (!CheckNegativeTraits(characterTraits.GetNegativeTraits(),
-                    taskCondition.BaseCondition.Traits.GetNegativeTraits()))
+                    cond.Value.Traits.GetNegativeTraits()))
                 supplementaryCondition = false;
 
             if (!CheckSpaceshipTraits(spaceshipTraits, hiddenSpaceshipTraits,
-                    taskCondition.BaseCondition.SpaceshipTraits,
-                    taskCondition.BaseCondition.HiddenSpaceshipTraits))
+                    cond.Value.SpaceshipTraits,
+                    cond.Value.HiddenSpaceshipTraits))
                 supplementaryCondition = false;
 
             //Apply logical operator
@@ -72,6 +73,86 @@ public class ConditionSystem
         return validateCondition;
     }
 
+    public static bool CheckEnvironmentCondition(CharacterBehaviour[] crew, TraitsData.SpaceshipTraits spaceshipTraits, TraitsData.HiddenSpaceshipTraits hiddenSpaceshipTraits, ConditionSO condition)
+    {
+        bool validateCondition = true;
+        int validatingCrew = -1;
+        
+        validateCondition = CheckSpaceshipTraits(spaceshipTraits, hiddenSpaceshipTraits,
+            condition.BaseCondition.SpaceshipTraits,
+            condition.BaseCondition.HiddenSpaceshipTraits);
+
+        if (!validateCondition)
+            return false;
+        
+        for (int i = 0; i < crew.Length; i++)
+        {
+            //In theory you could check CheckJob return value, skipping an assignation each time, but if(!CheckJob) continue; seems to create unreachable code
+            validateCondition = CheckJob(crew[i].GetJob(), condition.BaseCondition.Traits.GetJob());
+            if (!validateCondition) continue;
+            
+            validateCondition = CheckPositiveTraits(crew[i].GetPositiveTraits(), condition.BaseCondition.Traits.GetPositiveTraits());
+            if (!validateCondition) continue;
+
+            validateCondition = CheckNegativeTraits(crew[i].GetNegativeTraits(), condition.BaseCondition.Traits.GetNegativeTraits());
+            if (!validateCondition) continue;
+
+            if (condition.supplementaryConditions.Count < 1)
+            {
+                if (!validateCondition)
+                    continue;
+                else
+                    return validateCondition;
+            }
+            
+            bool supplementaryCondition = true;
+            foreach (var cond in condition.supplementaryConditions)
+            {
+                supplementaryCondition = CheckJob(crew[i].GetJob(), cond.Value.Traits.GetJob());
+                if (!supplementaryCondition)
+                    break;
+                supplementaryCondition = CheckPositiveTraits(crew[i].GetPositiveTraits(),
+                    cond.Value.Traits.GetPositiveTraits());
+                if (!supplementaryCondition)
+                    break;
+                supplementaryCondition = CheckNegativeTraits(crew[i].GetNegativeTraits(),
+                    cond.Value.Traits.GetNegativeTraits());
+                if (!supplementaryCondition)
+                    break;
+                supplementaryCondition = CheckSpaceshipTraits(spaceshipTraits, hiddenSpaceshipTraits,
+                    cond.Value.SpaceshipTraits,
+                    cond.Value.HiddenSpaceshipTraits);
+                if (!supplementaryCondition)
+                    break;
+                
+                //Apply logical operator
+                switch (cond.Key)
+                {
+                    case TraitsData.TraitOperator.OR:
+                        validateCondition |= supplementaryCondition;
+                        break;
+                    case TraitsData.TraitOperator.AND:
+                        validateCondition &= supplementaryCondition;
+                        break;
+                    case TraitsData.TraitOperator.XOR:
+                        validateCondition ^= supplementaryCondition;
+                        break;
+                    case TraitsData.TraitOperator.NAND:
+                        validateCondition = !(validateCondition && supplementaryCondition);
+                        break;
+                    case TraitsData.TraitOperator.NOT:
+                        validateCondition = (validateCondition & !supplementaryCondition);
+                        break;
+                }
+            }
+
+            if (validateCondition)
+                break;
+        }
+
+        return validateCondition;
+    }
+    
     private static bool CheckJob(TraitsData.Job job, TraitsData.Job conditionJob)
     {
         return (job & conditionJob) == conditionJob;
