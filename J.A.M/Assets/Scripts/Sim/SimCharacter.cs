207269689c684fa4a0ef5c73501b71a56d9198ad
Alexandre <alexandremocoeur@gmail.com>
2024-01-06 15:52:46 +0100

    feat : fixed pathfinding

--- J.A.M/Assets/Scripts/Sim/SimCharacter.cs
+++ J.A.M/Assets/Scripts/Sim/SimCharacter.cs
@@ -1,13 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks.Triggers;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SimCharacter : MonoBehaviour
 {
-    public uint currentRoomDoor;
-    public uint targetRoomDoor;
-    public Stack<uint> doorPath;
-
+    [HideInInspector] public bool moveToTarget;
     
+    [HideInInspector] public uint currentRoomDoor = 6;
+    public Stack<uint> doorPath = new Stack<uint>();
+
+    private void Update()
+    {
+        Simulate();
+        
+        if (Input.GetKey(KeyCode.Space) && doorPath.Count == 0)
+        {
+            moveToTarget = true;
+            uint targetID = (uint)Random.Range(1, 4);
+            SimPathing.CreatePath(SimPathing.FindDoorByID(currentRoomDoor), SimPathing.FindDoorByID(4), this);
+        }
+    }
+
+    //todo : add movment towards furniture
+    //todo : scale w/ time tick
+    //todo : add fade between doors
+    //todo : implem vrai utilisation
+    //todo : assurer que tout marche avec plusieurs persos
+    //todo : stress test
+    public void Simulate()
+    {
+        if (!moveToTarget) return;
+        
+        //Add scaling based on TimeTickSystem
+
+        SimDoor door = SimPathing.FindDoorByID(currentRoomDoor);
+        if ((door.transform.position - transform.position).magnitude < .1f)
+        {
+            FadeToNextDoor();
+        }
+        else
+        {
+            MoveToCurrentDoor();
+        }
+    }
+
+    private void FadeToNextDoor()
+    {
+        if (doorPath.Count == 0)
+        {
+            moveToTarget = false;
+            return;
+        }
+        currentRoomDoor = doorPath.Pop();
+    }
+
+    private void MoveToCurrentDoor()
+    {
+        SimDoor door = SimPathing.FindDoorByID(currentRoomDoor);
+        Vector3 pos = transform.position;
+        pos = Vector3.Lerp(pos, door.transform.position, .0025f);
+        transform.position = pos;
+    }
+
+    public void SetDoorPath(Stack<uint> path)
+    {
+        doorPath = path;
+    }
 }
--- J.A.M/Assets/Scripts/Sim/SimPathing.cs
+++ J.A.M/Assets/Scripts/Sim/SimPathing.cs
@@ -4,51 +4,73 @@ using System.Collections.Generic;
 using System.IO.Enumeration;
 using System.Linq;
 using System.Xml.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = System.Random;
 
 public class SimPathing : MonoBehaviour
 {
-    [SerializeField] private static SimDoor[] doors;
+    public static SimPathing instance;
     
+    [SerializeField] private SimDoor[] doors;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        InitializeDoorIDs();
+    }
+
     //Call once at game start;
     public static void InitializeDoorIDs()
     {
-        for (uint i = 0; i < doors.Length; i++)
-        {
-            doors[i].doorID = i;
-        }
+        for (uint i = 0; i < instance.doors.Length; i++)
+            instance.doors[i].doorID = i+1; 
     }
     
-    public static void CreatePath(SimDoor characterDoor, SimDoor targetDoor)
+    public static void CreatePath(SimDoor characterDoor, SimDoor targetDoor, SimCharacter targetCharacter)
     {
+        if (characterDoor.doorID == targetDoor.doorID) return;
+        
         //Door hierarchy in current search
         Stack<SimDoor> doorStack = new Stack<SimDoor>();
         
         //Already visited doors
         List<uint> visitedIDs = new List<uint>();
 
-        if (FindPath(characterDoor, targetDoor, visitedIDs, doorStack)) Debug.Log("Path Found");
-        else Debug.Log("No Path To Target Door");
+        if (!FindPath(characterDoor, targetDoor, visitedIDs, doorStack)) 
+        {
+            Debug.LogError("No Path To Target Door");
+            return;
+        }
+        CreateIDStack(doorStack, targetCharacter);
+    }
+
+    private static void CreateIDStack(Stack<SimDoor> doorStack, SimCharacter targetCharacter)
+    {
+        targetCharacter.doorPath.Clear();
+        
+        Stack<uint> path = new Stack<uint>();
+        
+        while (doorStack.Count != 0)
+            path.Push(doorStack.Pop().doorID);
+        
+        targetCharacter.doorPath = path;
     }
 
     private static bool FindPath(SimDoor currentDoor, SimDoor targetDoor, List<uint> visitedIDs, Stack<SimDoor> doorStack)
     {
         visitedIDs.Add(currentDoor.doorID);
+        doorStack.Push(currentDoor);
         
         if (currentDoor.doorID == targetDoor.doorID) return true;
-
+        
         for (int i = 0; i < currentDoor.neighbours.Length; i++)
         {
-            if(visitedIDs.Contains(currentDoor.doorID)) continue;
-            
-            doorStack.Push(currentDoor);
-            
-            if (FindPath(currentDoor.neighbours[i], targetDoor, visitedIDs, doorStack))
-                return true;
-            
-            doorStack.Pop();
+            if (visitedIDs.Contains(currentDoor.neighbours[i].doorID)) continue;
+            if (FindPath(currentDoor.neighbours[i], targetDoor, visitedIDs, doorStack)) return true;
         }
-        
+
+        doorStack.Pop();
         return false;
     }
     
@@ -56,8 +78,7 @@ public class SimPathing : MonoBehaviour
     public static SimDoor FindDoorByID(uint doorID)
     {
         int index = 0;
-        //Invert comparison depending on post-compile comparison order
-        for (; (index <= doors.Length) || (doors[index].doorID == doorID); index++);
-        return index == doors.Length ? null : doors[index];
+        for (; (index != instance.doors.Length - 1) && (instance.doors[index].doorID != doorID); index++) ;
+        return index == instance.doors.Length ? null : instance.doors[index];
     }
 }
