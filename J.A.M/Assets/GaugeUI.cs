f3df08de11c1f5349777d410a3b9e4c9084155a3
ElSachs <75885665+ElSachs@users.noreply.github.com>
2024-01-18 18:23:25 +0100

    fix du merge

--- J.A.M/Assets/GaugeUI.cs
+++ J.A.M/Assets/GaugeUI.cs
@@ -6,9 +6,18 @@ public class GaugeUI : HoverableObject
     public SystemType systemType;
     public bool IsPreviewing;
 
-    public abstract void UpdateGauge(float value, float previewValue);
+    public virtual void UpdateGauge(float value, float previewValue)
+    {
+        
+    }
 
-    public abstract void PreviewOutcomeGauge(float value);
+    public virtual void PreviewOutcomeGauge(float value)
+    {
+        
+    }
 
-    public abstract void ResetPreviewGauge();
+    public virtual void ResetPreviewGauge()
+    {
+        
+    }
 }
