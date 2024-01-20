b8a574e1b96efb0bf83d60fb60c9ec122c7e18e3
Maxime <32262840+duflosky@users.noreply.github.com>
2024-01-20 16:22:00 +0100

    hotfix: popup progress;

--- J.A.M/Assets/StorylineSystem/Scripts/Popup.cs
+++ J.A.M/Assets/StorylineSystem/Scripts/Popup.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -33,19 +34,58 @@ namespace SS
 
         public void Initialize(string text, string title = null)
         {
-            if (title != null && popupTitle != null) popupTitle.text = title;
-            popupText.text = text;
             gameObject.SetActive(true);
+            if (title != null && popupTitle != null) popupTitle.text = title;
+            StartCoroutine(DisplayText(popupText, text, 0.02f));
             continueButtonPressed = false;
             if (passTutorialButton != null) passTutorialPressed = false;
         }
 
         public void InitializeEndGame(string text, string title)
         {
-            popupTitle.text = title;
-            popupText.text = text;
             gameObject.SetActive(true);
+            popupTitle.text = title;
+            StartCoroutine(DisplayText(popupText, text, 0.02f));
             continueButton.onClick.AddListener(() => GameManager.Instance.MenuManager.LoadScene("MenuScene"));
+        }
+        
+        private IEnumerator DisplayText(TextMeshProUGUI text, string textToDisplay, float speed)
+        {
+            int letterIndex = 0;
+            string tempText = "";
+            string tagBuffer = "";
+            bool bufferTag = false;
+            text.text = tempText;
+
+            while (letterIndex < textToDisplay.Length)
+            {
+                //If tag-beginning character is parsed, start buffering the tag
+                if (textToDisplay[letterIndex] == '<')
+                    bufferTag = true;
+                //If tag-ending character is parsed, buffer the tag ending character and concatenate with text
+                if (textToDisplay[letterIndex] == '>')
+                {
+                    bufferTag = false;
+                    tagBuffer += textToDisplay[letterIndex];
+                    tempText = string.Concat(tempText, tagBuffer);
+                    letterIndex++;
+                    tagBuffer = ""; //Reset buffer in case of multiple tags.
+                    continue;
+                }
+
+                //If buffering tag, write to buffer instead of tempText and skip waiting
+                if (bufferTag)
+                {
+                    tagBuffer += textToDisplay[letterIndex];
+                    letterIndex++;
+                    continue;
+                }
+
+                yield return new WaitForSeconds(speed);
+                tempText += textToDisplay[letterIndex];
+                text.text = tempText;
+                letterIndex++;
+            }
         }
     }
 }
