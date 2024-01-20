484d631cb05e7003d5d94d7b703f01ae2adb575e
Maxime <32262840+duflosky@users.noreply.github.com>
2024-01-20 12:24:02 +0100

    feat: active raycast;

--- J.A.M/Assets/Scripts/GaugeUI.cs
+++ J.A.M/Assets/Scripts/GaugeUI.cs
@@ -1,9 +1,11 @@
 using System.Linq;
+using UnityEngine;
 
 public abstract class GaugeUI : HoverableObject
 {
     public SystemType systemType;
     public bool IsPreviewing;
+    public GameObject parentGauge;
 
     public void InitializeGauge()
     {
@@ -11,7 +13,7 @@ public abstract class GaugeUI : HoverableObject
         {
             text1 = systemType.ToString(),
             text2 = "Decrease : " + GameManager.Instance.SpaceshipManager.systems.First(system => system.type == systemType).decreaseSpeed,
-            baseParent = transform.parent,
+            baseParent = parentGauge.transform,
             parent = transform
         };
         hoverMenu.Initialize(data);
